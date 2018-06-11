62235dc9ffc5bb516946287509c0cb5be38e4dc2
qianyang.xie <qianyang.xie@vipshop.com>
2018-06-11 18:59:58 +0800

    fix webpack config

---
+++ src/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <h1>hello world</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body{
+  margin: 0
+}
+a {
+  text-decoration: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
